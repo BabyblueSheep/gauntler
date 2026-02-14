920f2903a52834f059fc3fc83a54cec47d2e51f4
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-02-14 20:16:12 +0200

    I HATE GRID ENTITIES

--- main.lua
+++ main.lua
@@ -9,7 +9,6 @@ include("scripts.utility.math")
 TheGauntlet.GauntletRoom = {}
 include("scripts.gauntlet_room.common")
 include("scripts.gauntlet_room.backdrop")
-include("scripts.gauntlet_room.door")
 include("scripts.gauntlet_room.chance")
 include("scripts.gauntlet_room.generation")
 include("scripts.gauntlet_room.waves")
