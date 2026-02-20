70770ed37e44afd894adb14626d776cc1af73f05
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-02-21 01:31:25 +0200

    Update default.lua

--- scripts/items/zeus/cases/default.lua
+++ scripts/items/zeus/cases/default.lua
@@ -5,8 +5,12 @@ return function (configItem, player, slot)
     if configItem.ChargeType == 2 then
         return 0
     elseif configItem.ChargeType == 1 then
-        return math.ceil((player:GetActiveCharge(slot)) / 45)
+        local chargeAmount = player:GetActiveCharge(slot)
+        if chargeAmount == 0 then return chargeAmount end
+        return math.ceil(chargeAmount / 45)
     else
-        return player:GetActiveCharge(slot) + 2
+        local chargeAmount = player:GetActiveCharge(slot)
+        if chargeAmount == 0 then return chargeAmount end
+        return chargeAmount + 2
     end
 end
