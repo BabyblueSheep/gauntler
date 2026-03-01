401eaa2310eabc01204b5ded9015fd0ec25779f4
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-03-02 01:18:09 +0200

    accurate blurbs compat + begin cleaning up

--- scripts/items/aphrodite.lua
+++ scripts/items/aphrodite.lua
@@ -21,6 +21,7 @@ TheGauntlet:AddCallback(ModCallbacks.MC_POST_ENTITY_TAKE_DMG, function (_, entit
     if source.Entity:IsActiveEnemy() then
         enemyToCharm = source.Entity
     else
+        --Allow projectiles and other non-NPC entities to charm the enemy (hopefully this doesn't have major flaws..)
         if source.Entity.SpawnerEntity ~= nil and source.Entity.SpawnerEntity:IsActiveEnemy() then
             enemyToCharm = source.Entity.SpawnerEntity
         elseif source.Entity.Parent ~= nil and source.Entity.Parent:IsActiveEnemy() then
@@ -37,22 +38,25 @@ TheGauntlet:AddCallback(ModCallbacks.MC_POST_ENTITY_TAKE_DMG, function (_, entit
     (
         0, 0, 0, 1,
         1, 0.66, 1
-    )
+    ) --Light pink color
+    local poofSize = Vector.One * (0.5 + enemyToCharm.Size / 144) --144 is roughly how big the poof is; the '0.5 +'' is to make the poof scale less with enemy size
+
     local poof1 = Isaac.Spawn
     (
         EntityType.ENTITY_EFFECT, EffectVariant.POOF02, 1,
         enemyToCharm.Position, Vector.Zero,
         nil
     )
-    poof1.SpriteScale = poof1.SpriteScale * 0.5
+    poof1.SpriteScale = poofSize
     poof1:GetSprite().Color = poofColor
+
     local poof2 = Isaac.Spawn
     (
         EntityType.ENTITY_EFFECT, EffectVariant.POOF02, 2,
         enemyToCharm.Position, Vector.Zero,
         nil
     )
-    poof2.SpriteScale = poof2.SpriteScale * 0.5
+    poof2.SpriteScale = poofSize
     poof2:GetSprite().Color = poofColor
 
     sfxManager:Play(SoundEffect.SOUND_BLACK_POOF)
