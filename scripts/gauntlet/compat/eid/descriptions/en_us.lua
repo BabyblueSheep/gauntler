20b5854f459e141755864001254ecec0e5233673
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-04-22 19:03:02 +0300

    Create en_us.lua

---
+++ scripts/gauntlet/compat/eid/descriptions/en_us.lua
@@ -0,0 +1,57 @@
+return {
+    ["item.aphrodite.name"] = "Aphrodite",
+    ["item.aphrodite.description"] =
+        "{{Friendly}} Taking damage from an enemy converts it to a friendly companion",
+
+    ["item.apollo.name"] = "Apollo",
+    ["item.apollo.description"] =
+        "Blocks projectiles#"..
+        "When hit, [1]% chance to grant triple shot for [2] seconds",
+
+    ["item.ares.name"] = "Ares",
+    ["item.ares.description"] =
+        "{{ChallengeRoom}} Spawns an additional Challenge Room each even numbered floor#"..
+        "{{BossRushRoom}} Spawns an additional Boss Challenge Room each odd numbered floor#"..
+        "Clearing a {{ChallengeRoom}} Challenge Room grants ↑ {{Damage}} +[1] Damage for the floor#"..
+        "Clearing a {{BossRushRoom}} Boss Challenge Room grants ↑ {{Damage}} +[2] Damage for the floor",
+
+    ["item.artemis.name"] = "Artemis",
+    ["item.artemis.description"] =
+        "An arrow facing a cardinal direction appears above Isaac that periodically rotates#"..
+        "Tears shot in the arrow's direction piece and gain a shot speed + [1]x damage boost",
+
+    ["item.athena.name"] = "Athena",
+    ["item.athena.description"] =
+        "Grants [1] rotating shields around Isaac"..
+        "Shields reflect projectiles and knock enemies back#"..
+        "{{Timer}} Shields go on a [2] second cooldown after deflecting",
+
+    ["item.demeter.name"] = "Demeter",
+    ["item.demeter.description"] =
+        "Causes a room-wide effect that cycles every room:#"..
+        "{{GauntletDemeterWinter}} Enemies are frozen on death#"..
+        "{{GauntletDemeterSpring}} [1]% chance to shoot a sticky booger#"..
+        "{{GauntletDemeterSummer}} Enemies constantly burn#"..
+        "{{GauntletDemeterAutumn}} Enemies are permanently slowed down",
+
+    ["item.dionysus.name"] = "Dionysus",
+    ["item.dionysus.description"] =
+        "↑ {{Heart}} +[1] Health#"..
+        "↑ {{Speed}} +[2] Speed#"..
+        "↑ {{Tears}} +[3] Tears#"..
+        "↑ {{Damage}} +[4] Damage#"..
+        "↑ {{Range}} +[5] Range#"..
+        "↑ {{Luck}} +[6] Luck#"..
+        "{{Timer}} Taking damage makes Isaac's movement slippery and distorts the screen for [7] seconds",
+
+    ["item.hades.name"] = "Hades",
+    ["item.hades.description"] =
+        "{{GauntletHadesStatusEffect}} [1]% chance to shoot a bone tear that inflicts Calcified#"..
+        "{{Friendly}} Killing a Calcified enemy spawns a friendly Bony",
+
+    ["item.hephaestus.name"] = "Hephaestus",
+    ["item.hephaestus.description"] =
+        "{{Trinket}} Spawns 1 random golden trinket#"..
+        "Entering a new floor spawns a random golden pickup",
+    
+}
