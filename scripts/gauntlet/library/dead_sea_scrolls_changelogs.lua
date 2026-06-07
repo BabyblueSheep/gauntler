15fa5b7a5edd346714840d9d06d142778b56f31c
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-06-07 15:47:59 +0300

    mod release + patch

--- scripts/gauntlet/library/dead_sea_scrolls_changelogs.lua
+++ scripts/gauntlet/library/dead_sea_scrolls_changelogs.lua
@@ -1,3 +1,11 @@
+DeadSeaScrollsMenu.AddChangelog("The Gauntlet", "1.0.1",
+[[
+{FSIZE3}1.0.1
+
+{FSIZE2}Compatibility Fixes
+- Fix an issue with StageAPI support
+]])
+
 DeadSeaScrollsMenu.AddChangelog("The Gauntlet", "1.0",
 [[
 {FSIZE3}1.0 : Release
